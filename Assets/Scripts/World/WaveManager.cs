f208a23096d22b03fb4013c0b37f3f0edb3d3e2d
miavies <michaelasantiago25@gmail.com>
2025-04-02 21:26:18 +0800

    Update WaveManager.cs

--- Assets/Scripts/World/WaveManager.cs
+++ Assets/Scripts/World/WaveManager.cs
@@ -4,9 +4,10 @@ public class WaveManager : MonoBehaviour
 {
     public int waveNumber = 1;
     public int customerNumber = 6;
-    public float comboMealChance = 3;
     public float lineWaitTime = 120f;
     public float NPCSeatedWaitTime = 60f;
+    public float comboMealChance = 3;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
